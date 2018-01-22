21ccae6817cddb0fb58bbb48b17ac9d7185a0c65
Michael Bartling <michael.bartling@arm.com>
2018-01-22 16:28:11 -0600

    It works with code gen. Need minor changes to the code gen header guards. Dont tell strostroup that I am manually destroying classes I have instantiated. He will be sad.

--- main.cpp
+++ main.cpp
@@ -33,33 +33,28 @@ void clear(Image<T>& img){
 template<typename T>
 void printImage(const Image<T>& img){
 
-    //pc.printf("\n\n{\n");
     for(int i = 0; i < img.get_xDim(); i++){
-        //pc.printf("[ ");
         for(int j = 0; j < img.get_yDim(); j++){
             pc.printf("%f, ", img(i,j));
         }
         pc.printf("]\n\r");
     }
-    //pc.printf("}\n");
-
 }
+
+/**
+ * Simple box filter with extra weight on the center element.
+ * Blurs the image to make it more realistic.
+ */
 template<typename T>
 Image<T> box_blur(const Image<T>& img){
     Image<T> tmp(img.get_xDim(), img.get_yDim());
     clear(tmp);
     for(int i = 4; i < img.get_xDim() - 4; i++){
         for(int j = 4; j < img.get_yDim() - 4; j++){
-            //for(int ii = -1; ii < 2; ii++){
-            //    for(int jj = -1; jj < 2; jj++){
-            //        tmp(i,j) += img(i+ii, j+jj);
-            //    }
-            //}
             tmp(i,j) = img(i-1, j-1) + img(i, j-1) + img(i+1, j-1) +
                        img(i-1, j) + 3.0*img(i, j) + img(i+1, j) + 
                        img(i-1, j+1) + img(i, j+1) + img(i+1, j+1);
             tmp(i,j) /= 11.0;
-            //tmp(i,j) /= 9.0;
         }
     }
 
@@ -75,9 +70,6 @@ int main()
     ON_ERR(bd.init(), "SDBlockDevice init ");
     ON_ERR(fs.mount(&bd), "Mounting the filesystem on \"/fs\". ");
     Image<float>* img = new Image<float>(240, 240);
-
-   // int prediction = runMLP("/fs/testData/deep_mlp/import-Placeholder_0.idx");
-   // pc.printf("prediction: %d\r\n\r\n\r\n\r\n", prediction);
 
     BSP_LCD_Init();
     button.rise(&trigger_inference_cb);
@@ -108,39 +100,34 @@ int main()
             
             Image<float> smallImage = resize(*img, 64, 64);
 
-            //printImage(smallImage);
-            //printImage(*img);
 
             Image<float> chopped = chop(smallImage);
             pc.printf("Done chopping\n\n");
             Image<float> img20   = resize(chopped, 20, 20);
             pc.printf("Done resizing\n\n");
             Image<float> img28   = pad(img20, 4, 4);
-            //Image<float> img28   = pad(img20, 0, 0);
-            //printImage(img28);
+            
+            // Image processing is heavy on constrained devices
+            // manually delete
             pc.printf("Done padding\n\n");
             smallImage.~Image<float>();
             chopped.~Image<float>();
             img20.~Image<float>();
             delete img;
 
-            //smallImage = pad(resize(chop(smallImage), 20, 20), 4, 4);
             Image<float> img28_2 = box_blur(img28);
             pc.printf("Done blurring\n\r");
-            //img28.~Image<float>();
+            img28.~Image<float>();
 
-            //printImage(img28_2);
             pc.printf("Reshaping\n\r");
-            img28.get_data()->resize({1, 784});
+            img28_2.get_data()->resize({1, 784});
             pc.printf("Creating Graph\n\r");
 
-            get_deep_mlp_ctx(ctx, img28.get_data());
+            get_deep_mlp_ctx(ctx, img28_2.get_data());
             pc.printf("Evaluating\n\r");
             ctx.eval();
             S_TENSOR prediction = ctx.get({"y_pred:0"});
             int result = *(prediction->read<int>(0,0));
-            //printImage(img28);
-            //int result = runMLP(img28.get_data());
             
             pc.printf("Number guessed %d\n\r", result);
 
@@ -162,8 +149,7 @@ int main()
             /* Get X and Y position of the first touch post calibrated */
             x1 = TS_State.touchX[0];
             y1 = TS_State.touchY[0];
-            //printf("Touch Detected x=%d y=%d\n", x1, y1);
-            //(*img)((int) x1, (int) y1) = 255;
+            
             img->draw_circle(x1, y1, 7); //Screen not in image x,y format. Must transpose
 
             BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
