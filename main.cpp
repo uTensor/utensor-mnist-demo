f544fb9e052567c72dd009ee200a2ab9047e2c65
Jan Jongboom <janjongboom@gmail.com>
2018-04-04 12:30:19 +0200

    Add mbed-simulator support

--- main.cpp
+++ main.cpp
@@ -2,16 +2,20 @@
 #include "stm32f413h_discovery.h"
 #include "stm32f413h_discovery_ts.h"
 #include "stm32f413h_discovery_lcd.h"
-#include "FATFileSystem.h"
-#include "F413ZH_SD_BlockDevice.h"
 #include "tensor.hpp"
 #include "image.h"
 #include "deep_mnist_mlp.hpp"
 #include "models/deep_mlp.hpp"
 
+#ifndef TARGET_SIMULATOR
+#include "FATFileSystem.h"
+#include "F413ZH_SD_BlockDevice.h"
 Serial pc(USBTX, USBRX, 115200);
 F413ZH_SD_BlockDevice bd;
 FATFileSystem fs("fs");
+#else
+#define USER_BUTTON     BUTTON1
+#endif
 
 InterruptIn button(USER_BUTTON);
 
@@ -26,7 +30,7 @@ void clear(Image<T>& img){
     for(int i = 0; i < img.get_xDim(); i++){
         for(int j = 0; j < img.get_yDim(); j++){
             img(i,j) = 0;
-        }   
+        }
     }
 }
 
@@ -35,9 +39,9 @@ void printImage(const Image<T>& img){
 
     for(int i = 0; i < img.get_xDim(); i++){
         for(int j = 0; j < img.get_yDim(); j++){
-            pc.printf("%f, ", img(i,j));
+            printf("%f, ", img(i,j));
         }
-        pc.printf("]\n\r");
+        printf("]\n\r");
     }
 }
 
@@ -52,7 +56,7 @@ Image<T> box_blur(const Image<T>& img){
     for(int i = 4; i < img.get_xDim() - 4; i++){
         for(int j = 4; j < img.get_yDim() - 4; j++){
             tmp(i,j) = img(i-1, j-1) + img(i, j-1) + img(i+1, j-1) +
-                       img(i-1, j) + 3.0*img(i, j) + img(i+1, j) + 
+                       img(i-1, j) + 3.0*img(i, j) + img(i+1, j) +
                        img(i-1, j+1) + img(i, j+1) + img(i+1, j+1);
             tmp(i,j) /= 11.0;
         }
@@ -65,10 +69,15 @@ Image<T> box_blur(const Image<T>& img){
 int main()
 {
     uint16_t x1, y1;
-    pc.printf("program start...\r\n");
+    printf("uTensor deep learning character recognition demo\n");
+    printf("https://github.com/uTensor/utensor-mnist-demo\n");
+    printf("Draw a number (0-9) on the touch screen, and press the button...\r\n");
 
+#ifndef TARGET_SIMULATOR
     ON_ERR(bd.init(), "SDBlockDevice init ");
     ON_ERR(fs.mount(&bd), "Mounting the filesystem on \"/fs\". ");
+#endif
+
     Image<float>* img = new Image<float>(240, 240);
 
     BSP_LCD_Init();
@@ -97,39 +106,39 @@ int main()
     while (1) {
         BSP_TS_GetState(&TS_State);
         if(trigger_inference){
-            
+
             Image<float> smallImage = resize(*img, 64, 64);
 
 
             Image<float> chopped = chop(smallImage);
-            pc.printf("Done chopping\n\n");
+            printf("Done chopping\n\n");
             Image<float> img20   = resize(chopped, 20, 20);
-            pc.printf("Done resizing\n\n");
+            printf("Done resizing\n\n");
             Image<float> img28   = pad(img20, 4, 4);
-            
+
             // Image processing is heavy on constrained devices
             // manually delete
-            pc.printf("Done padding\n\n");
+            printf("Done padding\n\n");
             smallImage.~Image<float>();
             chopped.~Image<float>();
             img20.~Image<float>();
             delete img;
 
             Image<float> img28_2 = box_blur(img28);
-            pc.printf("Done blurring\n\r");
+            printf("Done blurring\n\r");
             img28.~Image<float>();
 
-            pc.printf("Reshaping\n\r");
+            printf("Reshaping\n\r");
             img28_2.get_data()->resize({1, 784});
-            pc.printf("Creating Graph\n\r");
+            printf("Creating Graph\n\r");
 
             get_deep_mlp_ctx(ctx, img28_2.get_data());
-            pc.printf("Evaluating\n\r");
+            printf("Evaluating\n\r");
             ctx.eval();
             S_TENSOR prediction = ctx.get({"y_pred:0"});
             int result = *(prediction->read<int>(0,0));
-            
-            pc.printf("Number guessed %d\n\r", result);
+
+            printf("Number guessed %d\n\r", result);
 
             BSP_LCD_Clear(LCD_COLOR_WHITE);
             BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
@@ -149,7 +158,7 @@ int main()
             /* Get X and Y position of the first touch post calibrated */
             x1 = TS_State.touchX[0];
             y1 = TS_State.touchY[0];
-            
+
             img->draw_circle(x1, y1, 7); //Screen not in image x,y format. Must transpose
 
             BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
@@ -158,7 +167,4 @@ int main()
             wait_ms(5);
         }
     }
-
-    ON_ERR(fs.unmount(), "fs unmount ");
-    ON_ERR(bd.deinit(), "SDBlockDevice de-init ");
 }
